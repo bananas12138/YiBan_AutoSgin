95d22099e31d501f4a62064b8bd07d441a678b8f
bananas12138 <49580783+bananas12138@users.noreply.github.com>
2022-09-24 23:49:50 +0800

    Add files via upload

---
+++ src/userData.py
@@ -0,0 +1,12 @@
+user_data = [{
+	'Phone': '13126011292',
+	'PassWord': '2013320145qwe',
+	'SendKey': 'SCT35713TwzPSXm1IaD6EfJ7PuiW8kVty', # 用于微信通知, 在Server酱官网注册用户获取
+	'Address': '定位地址' 
+},# 第二个人. 如果不需要, 只保留,号前的内容和最后的]号就行
+{
+	'Phone': '手机号',
+	'PassWord': '密码(明文)',
+	'SendKey': 'Server酱 Token Key',
+	'Address': '定位地址' 
+}]
